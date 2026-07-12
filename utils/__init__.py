896fa4bf49dcd6d358e622485b1d4820c68c54c7
developerhizol <kostatrofimcev11@gmail.com>
2026-07-12 13:14:59 +0300

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .states import StarsStates, PremiumStates, GiftsStates
-from .database import init_db, get_user, create_user, update_user, add_purchase, complete_purchase, get_user_purchases, get_user_stats
